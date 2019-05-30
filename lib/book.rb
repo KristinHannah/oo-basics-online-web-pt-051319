b5974a737f6fd01416674ee29934d662939cc146
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-30 23:27:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,9 +7,15 @@ class Book
     @title = title 
   end 
   
+  def title=(title)
+    @title = title 
+  end 
   
+
   def turn_page 
     puts "Flipping the page...wow, you read fast!"
   end 
+  
+  
 end 
 
